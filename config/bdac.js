1c23b15ec9b426f61f5d907e2d1dd6174f1854a1
Short <51891843+short443@users.noreply.github.com>
2023-02-14 23:42:51 -0300

    Update v1.0.8

--- config/bdac.js
+++ config/bdac.js
@@ -1,48 +1,37 @@
-/**
- * An array of API keys and their corresponding secrets.
- * @type {Array<{key: string, secret: string}>}
- */
+// An array of API keys and their corresponding secrets.
+
 let apiKeys = [
-{
-    key: "",
-    secret: ""
-},
-{
-    key: "",
-    secret: ""
-},
-{
-    key: "",
-    secret: ""
-},
+    {
+        key: "",
+        secret: ""
+    },
+    {
+        key: "",
+        secret: ""
+    },
+    {
+        key: "",
+        secret: ""
+    },
 ],
 
-/**
-* A function that returns a CORS-enabled URL for the given URL.
-* @param {string} url - The URL to convert into a CORS-enabled URL.
-* @returns {string} The CORS-enabled URL.
-*/
+// A function that returns a CORS-enabled URL for the given URL. Default: "https://corsproxy.io/?".
 
 cors_api_url = "https://corsproxy.io/?",
 
-/**
-* A filter that will only show domains with a price greater than the given value.
-* @param {number} highValuePrice - the high value price to filter products by.
-* @returns {FilterConfig} - a filter config object.
-*/
+
+// highValuePriceFilter diffines the value of the domains that will be recognized as "HighValue Domains".
+// These will appear in a separate <div> for better viewing. Default: 2000.
 
 highValuePriceFilter = 2000,
 
-/**
-* Beep sound to notify about high value domains. (To active, change the value to "true")
-* @returns None           
-*/
+
+// Beep sound to notify about high value domains. Default: false.
 
 highValueBeep = false,
 
-/**
-* General global variables.             
-*/
+// General global variables.
+
 checkedDomains = new Set(),
 index = 0,
 keyIndex = 0,
@@ -57,6 +46,7 @@ notyf = new Notyf(),
 domainsData = [];
 
 // Formula that calculates the number of keys and defines the optimal delay.
+
 let countOfApiKeys = 0;
 for (let i = 0; i < apiKeys.length; i++) {
 let key = apiKeys[i];
@@ -67,13 +57,24 @@ countOfApiKeys++;
 let result = countOfApiKeys * 20;
 let delay = 60000 / result;
 
-/**
-* Appraises the domains in the text area.
-* @returns None
-*/
+// Appraises the domains in the text area.
+
 function appraiseDomains() {
 sortEnabled = false;
+
+// Checks if there is content in the <div id="result">
+// If there is, the program will warn the user before closing or restarting the page.
+
+setInterval(function() {
+    if (document.getElementById("result").innerHTML) {
+      document.body.setAttribute("onbeforeunload", "return progressAlert()");
+    } else {
+      document.body.removeAttribute("onbeforeunload");
+    }
+  }, 300);
+
 // Check if the variable "cors_api_url" is set, if not, do not start checking.
+
 if (!cors_api_url) {
 notyf
     .error({
@@ -85,6 +86,7 @@ return
 }
 
 // Check if the variable "apiKey" is set, if not, do not start checking.
+
 else if (!apiKeys.some((apiKey) => apiKey.key && apiKey.secret)) {
 notyf
     .error({
@@ -96,38 +98,51 @@ return
 }
 
 // Set variable "e" the values in text area "textarea-form5-k".
+
 var e = document.getElementById("textarea-form5-k");
 
 if (e.value) {
-// Disable "Evaluate" and "Sort" buttons to avoid data conflicts.
+
+    // Disable "Evaluate" and "Sort" buttons to avoid data conflicts.
+
 document.querySelector(".btn-checked").disabled = true;
 document.querySelector(".btn-checked").title = "The button has been deactivated, stop the appraisal before starting another.";
 document.querySelector(".btn-sort").disabled = true;
 document.querySelector(".btn-sort").title = "The button has been deactivated, to reactivate stop the appraisal process.";
 
 // Removes spaces and empty lines before sending data for processing.
+
 domains = e.value.split("\n").map((domain) => {
     domain = domain.replace(/\s/g, "");
+    
     // Remove everything before '://'.
+    
     let index = domain.indexOf("://");
     if (index !== -1) {
         domain = domain.substring(index + 3);
     }
     // Remove '/' and anything after it from the end of the domain.
+    
     index = domain.indexOf("/");
     if (index !== -1) {
         domain = domain.substring(0, index);
     }
+    
     // In case of "Keyword" add "".com" at the end of the domain before processing the data.
+    
     if (domain.length > 0 && !domain.includes(".")) {
         domain += ".com";
     }
     return domain;
 });
+
 // Starts the check process by calling the function "checkNextDomain()".
+
 index = 0;
 checkNextDomain();
+
 // If there is no content in the text area the message below will be returned with no further check.
+
 } else {
 notyf
     .error({
@@ -139,19 +154,18 @@ return
 }
 }
 
-/**
-* Checks the next domain in the list of domains to check.
-* @returns None
-*/
+// Checks the next domain in the list of domains to check.
+
 function checkNextDomain() {
 
 // Gets the next domain from the domains array.
+
 if (!(index >= domains.length)) {
 var e = domains[index++];
 dn = e
 
+// Checks if the domain is in the checked domains set. * If it is, it checks the next domain.* If it isn't, it checks the domain.
 
-//Checks if the domain is in the checked domains set. * If it is, it checks the next domain.* If it isn't, it checks the domain.
 if (0 != e.trim().length) {
     if (checkedDomains.has(e)) {
         checkNextDomain();
@@ -159,6 +173,7 @@ if (0 != e.trim().length) {
         checkedDomains.add(e);
 
         // Finds the next API key and secret that are valid.           
+        
         while (!apiKeys[keyIndex].key || !apiKeys[keyIndex].secret) {
             keyIndex++;
             if (keyIndex === apiKeys.length) {
@@ -167,6 +182,7 @@ if (0 != e.trim().length) {
         }
 
         // Data to connect to the Godaddy API.
+        
         var t = cors_api_url + "https://api.godaddy.com/v1/appraisal/" + e;
         var n = {
             method: "GET",
@@ -175,12 +191,14 @@ if (0 != e.trim().length) {
             }
         };
 
-        // Increments the requests count and if it reaches 20, increments the keyIndex.
-
         // Fetch process and error handling.
+        
         var a = [];
         var fetchErrorCount = 0;
         var fetchData = function() {
+
+            // Increments the requests count and if it reaches 20, increments the keyIndex.
+            
             requestsCount++;
             if (20 === requestsCount) {
                 keyIndex++;
@@ -189,13 +207,21 @@ if (0 != e.trim().length) {
                     keyIndex = 0;
                 }
             }
+            
             // Fetch process t= request n= options
+            
             fetch(t, n)
+                
                 // Takes in a response object and returns the response body as a JSON object.
+                
                 .then(response => response.json())
+                
                 // A function that takes in a promise and returns a promise that resolves after a given amount of time.
+                
                 .then((e) => {
+                    
                     // in case of error code "TOO_MANY_REQUESTS" wait for the time informed in retryAfterSec in the request's response and try to check it again.
+                    
                     if (e.code === "TOO_MANY_REQUESTS") {
                         console.log(`Failed to fetch the domain: ${dn} Error code: ${e.code} Trying again in ${e.retryAfterSec} seconds.`);
                         notyf
@@ -208,12 +234,14 @@ if (0 != e.trim().length) {
                     }
 
                     // in case of error code "429" from CORS, wait 1 minute and try to check it again. ( I don't know if it works, I couldn't replicate this error to test. )
+                    
                     else if (e.code === 429) {
                         console.log(`Failed to fetch the domain: ${dn} Error code: ${e.code}`);
                         timeoutId = setTimeout(fetchData, 60000);
                     }
 
                     // in case of error status "UNSUPPORTED_DOMAIN" wait the delay time and skip it.
+                    
                     else if (e.status === "UNSUPPORTED_DOMAIN") {
                         console.log(`Failed to fetch the domain: ${dn} Error code: ${e.status}`);
                         notyf
@@ -226,6 +254,7 @@ if (0 != e.trim().length) {
                     }
 
                     // in case of error status "MASKED_DOMAIN" wait the delay time and skip it.
+                    
                     else if (e.status === "MASKED_DOMAIN") {
                         console.log(`Failed to fetch the domain: ${dn} Error code: ${e.status}`);
                         notyf
@@ -238,6 +267,7 @@ if (0 != e.trim().length) {
                     }
 
                     // in case of error status "NOT_FOUND" wait the delay time and skip it.
+                    
                     else if (e.code === "NOT_FOUND") {
                         console.log(`Failed to fetch the domain: ${dn} Error code: ${e.code}`);
                         notyf
@@ -250,6 +280,7 @@ if (0 != e.trim().length) {
                     }
 
                     // if the request return status = OK, format response data and present it to the user in html.
+                    
                     else if ("OK" === e.status) {
                         var domain = e.domain;
                         var govalue = e.govalue;
@@ -263,6 +294,7 @@ if (0 != e.trim().length) {
                         document.getElementById("result").insertAdjacentHTML("afterbegin", resultDiv.outerHTML);
 
                         // Captures domains with the price greater than or equal to the price set in the "highValuePriceFilter" variable and sends them to a different div in the user interface.
+                        
                         if (govalue >= highValuePriceFilter) {
                             a.push({
                                 domain: domain,
@@ -271,6 +303,7 @@ if (0 != e.trim().length) {
 
                             // Beep sound to notify about high value domains.
                             // ( To activate, change the value of the variable "highValueBeep" to true. )
+                            
                             if (highValueBeep === true) {
                                 var audio = new Audio("assets/audio/hvbeep.mp3");
                                 audio.volume = 0.25;
@@ -278,6 +311,7 @@ if (0 != e.trim().length) {
                             }
 
                             // Formatting high-value domains.
+                            
                             var highValueResultDiv = document.createElement("div");
                             var highValueText = "Domain: " + dn + "<br>" + "Value: $" + govalue;
                             highValueResultDiv.innerHTML = highValueText;
@@ -295,6 +329,7 @@ if (0 != e.trim().length) {
 
                     // In case of errors like 404, 502, 504 the program must try to re-fetch up to 3 times to return the data.
                     // If you don't get "status = OK" the program will skip informing the user that it was not possible to verify the domain.
+                    
                     else {
                         console.error(`Error: ${e.status}`);
                         while (fetchErrorCount < 3) {
@@ -332,13 +367,15 @@ if (0 != e.trim().length) {
 }
 
 		// Once the process of checking one domain is finished, the function is called again to check the next one.
-		else {
+		
+        else {
 			checkNextDomain();
 		}
 	}
 
 	// If all domains in the list are checked, the program will release the disabled buttons and send a completion message to the user
-	else if (domains.length === index) {
+	
+    else if (domains.length === index) {
 		sortEnabled = true;
 		notyf
 			.success({
@@ -350,13 +387,12 @@ if (0 != e.trim().length) {
 		document.querySelector(".btn-checked").title = "";
 		document.querySelector(".btn-sort").disabled = false;
 		document.querySelector(".btn-sort").title = "";
+        
 	}
 }
 
-/**
- * Exports the data in the table to a CSV file.
- * @returns None
- */
+// Exports the data in the table to a CSV file.
+
 function exportToCSV() {
 	let csv = "domain,value\n";
 	let result = document.getElementById("result");
@@ -378,10 +414,8 @@ function exportToCSV() {
 	hiddenElement.click();
 }
 
-/**
- * Sorts the domains by their value.
- * @returns None
- */
+// Sorts the domains by their value.
+
 function sortDomains() {
 	if (!sortEnabled) {
 		document.querySelector(".btn-sort").title = "The button has been deactivated, to reactivate stop the appraisal process.";
@@ -407,9 +441,8 @@ function sortDomains() {
 	}
 }
 
-/**
- * Clears the results of the previous scan.
- */
+// Clears the results of the previous scan.
+
 async function clearResults() {
 	await notyf.dismissAll();
 	(document.getElementById("highValueResult").innerHTML = ""),
@@ -418,8 +451,8 @@ async function clearResults() {
 	(oldInnerHTML = ""),
 	(domainsData = []),
 	(result = ""),
-	checkedDomains.clear(),
-		(index = 0);
+    (index = 0),
+	(checkedDomains.clear()),
 	document.querySelector(".btn-checked").disabled = false;
 	document.querySelector(".btn-checked").title = "";
 	document.querySelector(".btn-sort").disabled = false;
@@ -432,19 +465,15 @@ async function clearResults() {
 		});
 }
 
-/**
- * Clears all timeouts.
- * @returns None
- */
+// Clears all timeouts.
+
 function clearAllTimeouts() {
 	clearTimeout(timeoutId);
 	clearTimeout(retryTimeoutId);
 }
 
-/**
- * Copies the checked domains to the clipboard.
- * @returns None
- */
+// Copies the checked domains to the clipboard.
+
 async function copyToClipboard() {
 	try {
 		var result = document.getElementById("result");
@@ -458,10 +487,8 @@ async function copyToClipboard() {
 	}
 }
 
-/**
- * A function that clears all timeouts and intervals.
- * @returns None
- */
+// A function that clears all timeouts and intervals.
+
 window.addEventListener("click", function(event) {
 	if (event.target.id === "stop") {
 		var i = 0;
@@ -483,8 +510,15 @@ window.addEventListener("click", function(event) {
 		document.querySelector(".btn-checked").title = "";
 		document.querySelector(".btn-sort").disabled = false;
 		document.querySelector(".btn-sort").title = "";
+        document.body.removeAttribute("onbeforeunload");
 		sortEnabled = true;
 	}
 });
 
-// v1.0.7 Code Version - check https://github.com/short443/BDAC/releases/ for updates.
+// alert before closing or reloading the page.
+
+function progressAlert() {
+    return "Are you sure you want to leave? The results of the page will be lost.";
+  }
+
+// v1.0.8 Code Version - check https://github.com/short443/BDAC/releases/ for updates.
